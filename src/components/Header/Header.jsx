33da2099c9955843e34020d0b4b6b71b3261d86e
Azamjon62 <azamjonx499@gmail.com>
2023-04-07 12:49:47 +0500

    responsive commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./index.scss"
+import toggle1 from "../../assets/img/toggle.svg";
 
 const Header = () => {
   return (
@@ -9,6 +10,7 @@ const Header = () => {
           <li className="header__ul__li"><a className="header__ul__li-item" href="#">Works</a></li>
           <li className="header__ul__li"><a className="header__ul__li-item" href="#">Blog</a></li>
           <li className="header__ul__li"><a className="header__ul__li-item" href="#">Contact</a></li>
+          <a className='header__ul__toggle' href='#' ><img src={toggle1} alt="" /></a>
         </ul>
       </div>
     </header>
